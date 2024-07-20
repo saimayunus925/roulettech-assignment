73f40f4ffa5f169870b9f9525951bbbed4105071
Saima Yunus <saimayunus925@gmail.com>
2024-07-19 17:18:20 -0700

    added basic Django views

---
+++ backend/api/serializers.py
@@ -0,0 +1,5 @@
+# backend/api/serializers.py
+from rest_framework import serializers
+
+# If there are models, define serializers here
+# todo: add models and serializers later once the app is running
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
+
+# GET request for data to be printed to screen
+@api_view(['GET'])
+def get_data(request):
+    data = [
+        {'name': 'Item 1'},
+        {'name': 'Item 2'},
+        {'name': 'Item 3'},
+    ]
+    return Response(data)
+
+# GET request for message to be printed to screen
+@api_view(['GET'])
+def get_message(request):
+    message = "Hello from Django!"
+    return Response(message)
